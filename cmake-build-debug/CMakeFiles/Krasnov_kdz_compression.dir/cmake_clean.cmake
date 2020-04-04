54a9c874eae850526e4e74cd0c8331a6f86e4aca
Alexandr <genryxy.alexandr@yandex.ru>
2020-04-05 00:43:59 +0300

    Implemented the Huffman and LZ77 algorithms, raw version

---
+++ cmake-build-debug/CMakeFiles/Krasnov_kdz_compression.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Krasnov_kdz_compression.dir/main.cpp.obj"
+  "Krasnov_kdz_compression.pdb"
+  "Krasnov_kdz_compression.exe"
+  "Krasnov_kdz_compression.exe.manifest"
+  "libKrasnov_kdz_compression.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Krasnov_kdz_compression.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
